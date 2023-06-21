28e37964da5875c782374b0b63c39ad15b575631
Mateus <msouza954@gmail.com>
2023-06-21 16:29:34 -0300

    first commit

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components' //css para ficar com as cores do css
+
+//cores definidas na const buttonVariants
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+}
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+//um componente estilizados que esta o elemento button do html
+//para que o Button Container receba as props de variant colocar <ButtonContainerProps> depois do elemento HTML
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  //interpolação de string
+  //o styled component fará que com o codigo dessa interpolção seja executado como uma função
+  // essa prop serrá a variant
+  ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }}
+`
