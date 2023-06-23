661e7a328d478a0497b3f447f42af0d0ad974f0f
Mateus <msouza954@gmail.com>
2023-06-23 16:55:54 -0300

    aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <>
+    //o thema só vai ser aplicado para elementos que estiverem dendtro do ThemeProvider
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -19,13 +19,18 @@ interface ButtonContainerProps {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border: none;
+  border-radius: 8px;
+  margin: 8px;
 
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
   //interpolação de string
   //o styled component fará que com o codigo dessa interpolção seja executado como uma função
   // essa prop serrá a variant
-  ${(props) => {
+  /* ${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `
-  }}
+  }} */
 `
