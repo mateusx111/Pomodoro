e43f1068c76ed315464161ba31e56d9806d819be
Mateus Souza <msouza954@gmail.com>
2023-09-08 11:18:48 -0300

    up

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,13 +9,13 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px; ${(props) => props.theme['blue-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme['blue-500']};
   }
 
   body { 
     -webkit-font-smoothing: antialiased;
     background: ${(props) => props.theme['gray-100']};
-    color: ${(props) => props.theme['gray-900']}
+    color: ${(props) => props.theme['gray-900']};
   }
 
   body, input, textarea, button{
